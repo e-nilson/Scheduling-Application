46281d848b6077ffbd20d4013432824de4ddf392
Erik <68452795+e-nilson@users.noreply.github.com>
2022-02-04 19:35:57 -0800

    Update AppointmentDB.java

--- src/Utils/AppointmentDB.java
+++ src/Utils/AppointmentDB.java
@@ -4,10 +4,7 @@ import Model.Appointment;
 import Model.ReportByMonth;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Timestamp;
+import java.sql.*;
 
 /**
  * Appointment database.
@@ -16,10 +13,15 @@ import java.sql.Timestamp;
  */
 public class AppointmentDB {
 
-    public static void select() throws SQLException {
-        String sql = "SELECT * FROM appointments";
-        PreparedStatement ps = DBConnection.connection.prepareStatement(sql);
+    public static void getAllAppointments() throws SQLException {
+        //DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.S");
+
+        PreparedStatement ps = DBConnection.connection.prepareStatement("SELECT * FROM appointments");
         ResultSet rs = ps.executeQuery();
+
+        //Use LocalDateTime instead of Timestamp. .toLocalDateTime. Right now they are being displayed in UTC.
+        // need to get rid of timezone conversions
+        // zip up project to test in VM.
 
         while (rs.next()) {
             int appointment_ID = rs.getInt("Appointment_ID");
@@ -27,6 +29,8 @@ public class AppointmentDB {
             String description = rs.getString("Description");
             String location = rs.getString("Location");
             String type = rs.getString("Type");
+            //LocalDateTime start = rs.getTimestamp("Start").toLocalDateTime();
+            //LocalDateTime end = rs.getTimestamp("End").toLocalDateTime();
             Timestamp start = rs.getTimestamp("Start");
             Timestamp end = rs.getTimestamp("End");
             int customer_ID = rs.getInt("Customer_ID");
@@ -42,11 +46,12 @@ public class AppointmentDB {
      *
      * @throws SQLException
      */
-    public static boolean addAppointment(int appointment_id, String title, String description, String location, String type, Timestamp start, Timestamp end, int contact_id, int customer_id, int user_id) throws SQLException {
+    public static boolean addAppointment(int appointment_ID, String title, String description, String location, String type, Timestamp start, Timestamp end, int contact_ID, int customer_ID, int user_ID) throws SQLException {
         try {
-            String sql1 = ("INSERT INTO appointments SET Appointment_ID='" + appointment_id + "',Title='" + title + "',Description='" + description + "', Location='" + location + "', Type='" + type + "', Start='" + start + "', End='" + end + "', Customer_ID='" + customer_id + "', User_ID='" + user_id + "', Contact_ID=" + contact_id);
+            String sql1 = ("INSERT INTO appointments SET Appointment_ID='" + appointment_ID + "',Title='" + title + "',Description='" + description + "', Location='" + location + "', Type='" + type + "', Start='" + start + "', End='" + end + "', Customer_ID='" + customer_ID + "', User_ID='" + user_ID + "', Contact_ID=" + contact_ID);
             PreparedStatement ps1 = DBConnection.connection.prepareStatement(sql1);
             ps1.executeUpdate(sql1);
+
         } catch (SQLException exc) {
             System.out.println("SQLException: " + exc.getMessage());
         }
@@ -58,11 +63,12 @@ public class AppointmentDB {
      *
      * @throws SQLException
      */
-    public static boolean updateAppointment(int appointment_id, String title, String description, String location, String type, Timestamp start, Timestamp end, int contact_id, int user_id, int customer_id) throws SQLException {
+    public static boolean updateAppointment(int appointment_ID, String title, String description, String location, String type, Timestamp start, Timestamp end, int contact_ID, int user_ID, int customer_ID) throws SQLException {
         try {
-            String sql2 = ("UPDATE appointments SET Title='" + title + "',Description='" + description + "', Location='" + location + "', Type='" + type + "', Start='" + start + "', End='" + end + "', User_ID='" + user_id + "', Contact_ID='" + contact_id + "', Customer_ID='" + customer_id + "' WHERE Appointment_ID =" + appointment_id);
+            String sql2 = "UPDATE appointments SET Title='" + title + "',Description='" + description + "', Location='" + location + "', Type='" + type + "', Start='" + start + "', End='" + end + "', User_ID='" + user_ID + "', Contact_ID='" + contact_ID + "', Customer_ID='" + customer_ID + "' WHERE Appointment_ID =" + appointment_ID;
             PreparedStatement ps2 = DBConnection.connection.prepareStatement(sql2);
             ps2.executeUpdate(sql2);
+
         } catch (SQLException exc) {
             System.out.println("SQLException: " + exc.getMessage());
         }
@@ -76,7 +82,6 @@ public class AppointmentDB {
      * @return returns true or false.
      */
     public static boolean deleteAppointment(int appointment_ID) {
-
         try {
             String query1 = "DELETE FROM appointments WHERE Appointment_ID =" + appointment_ID;
             PreparedStatement ps1 = DBConnection.connection.prepareStatement(query1);
@@ -113,5 +118,23 @@ public class AppointmentDB {
 
         }
         return monthAndTypeReport;
+    }
+
+    public static Appointment appointmentOverlap(Timestamp start, Timestamp end, int customer_ID) throws SQLException{
+        Appointment apptOverlap = null;
+
+        ObservableList<Appointment> apptList = CustomerDB.getCustAppt(customer_ID);
+
+        for (Appointment appointment : apptList) {
+            if (start.after(appointment.getStart()) && start.before(appointment.getEnd()) ||
+                    end.after(appointment.getStart()) && end.before(appointment.getEnd()) ||
+                    start.before(appointment.getStart()) && end.after(appointment.getStart()) ||
+                    start.equals(appointment.getStart()) && end.equals(appointment.getEnd()) ||
+                    start.equals(appointment.getStart()) || end.equals(appointment.getStart())) {
+
+                apptOverlap = appointment;
+            }
+        }
+        return apptOverlap;
     }
 }
